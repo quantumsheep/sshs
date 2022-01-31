6fcbb8aa1b8ef8a6ef1e9c02419a91e68d13ebc6
quantumsheep <nathanael.dmc@outlook.fr>
2022-02-01 00:58:29 +0100

    Implement sshs basics

---
+++ cmd/root.go
@@ -0,0 +1,231 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+	"strings"
+
+	"github.com/mikkeloscar/sshconfig"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+	tb "github.com/nsf/termbox-go"
+)
+
+func connect(name string) {
+	cmd := exec.Command("ssh", strings.TrimSpace(name))
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		os.Exit(cmd.ProcessState.ExitCode())
+	}
+
+	os.Exit(0)
+}
+
+var RootCmd = &cobra.Command{
+	Use:   "sshs",
+	Short: "ssh clients manager",
+	Run: func(cmd *cobra.Command, args []string) {
+		// read ~/.ssh/config
+		filepath, e := homedir.Expand("~/.ssh/config")
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		hosts, e := sshconfig.ParseSSHConfig(filepath)
+		if e != nil {
+			log.Fatal(e)
+		}
+
+		if err := ui.Init(); err != nil {
+			log.Fatalf("failed to initialize termui: %v", err)
+		}
+		defer ui.Close()
+		tb.SetInputMode(tb.InputEsc)
+
+		searchBar := widgets.NewParagraph()
+		searchBar.Text = "Search..."
+		searchBar.PaddingLeft = 1
+		searchBar.PaddingRight = 1
+		// searchBar.BorderStyle.Fg = ui.ColorM
+		searchBar.TextStyle.Fg = ui.ColorYellow
+
+		table := widgets.NewTable()
+		table.TextAlignment = ui.AlignLeft
+		table.RowSeparator = false
+		table.FillRow = true
+		table.RowStyles[0] = ui.NewStyle(ui.ColorBlue, ui.ColorClear, ui.ModifierBold)
+
+		table.Rows = make([][]string, 1)
+		table.Rows[0] = []string{"Hostname", "User", "Host", "Port"}
+
+		for _, host := range hosts {
+			if host.HostName == "" {
+				continue
+			}
+
+			name := strings.Join(host.Host, " ")
+
+			if name[0] == '"' && name[len(name)-1] == '"' {
+				name = name[1 : len(name)-1]
+			}
+
+			row := []string{name, host.User, host.HostName, strconv.Itoa(host.Port)}
+			table.Rows = append(table.Rows, row)
+		}
+
+		for _, row := range table.Rows {
+			for i := range row {
+				row[i] = " " + row[i]
+			}
+		}
+
+		table.ColumnWidths = make([]int, len(table.Rows[0]))
+		padding := 1
+
+		for _, row := range table.Rows {
+			for i, cell := range row {
+				length := len(cell) + padding
+
+				if length > table.ColumnWidths[i] {
+					table.ColumnWidths[i] = length
+				}
+			}
+		}
+
+		grid := ui.NewGrid()
+
+		termWidth, termHeight := ui.TerminalDimensions()
+		grid.SetRect(0, 0, termWidth, termHeight)
+
+		searchBarRatio := 3 / float64(termHeight)
+
+		grid.Set(
+			ui.NewCol(1.0,
+				ui.NewRow(searchBarRatio, searchBar),
+				ui.NewRow(1.0-searchBarRatio, table),
+			),
+		)
+
+		selectedHost := 0
+		table.RowStyles[selectedHost+1] = ui.NewStyle(ui.ColorBlack, ui.ColorWhite, ui.ModifierClear)
+
+		ui.Render(grid)
+
+		sum := 0
+		for i := 0; i < len(table.ColumnWidths)-1; i++ {
+			sum += table.ColumnWidths[i]
+		}
+
+		table.ColumnWidths[len(table.ColumnWidths)-1] = table.Inner.Dx() - sum
+
+		ui.Render(grid)
+
+		rows := table.Rows[1:]
+		previousSearch := ""
+		search := ""
+
+		previousKey := ""
+		uiEvents := ui.PollEvents()
+		for {
+			newSelectedHost := selectedHost
+
+			e := <-uiEvents
+			switch e.ID {
+			case "<C-c>":
+				return
+			case "<Enter>":
+				ui.Close()
+				connect(rows[selectedHost][0])
+				return
+			case "<Down>":
+				newSelectedHost += 1
+			case "<Up>":
+				newSelectedHost -= 1
+			case "<Backspace>":
+				if len(search) > 0 {
+					search = search[:len(search)-1]
+				}
+			case "<Space>":
+				search += " "
+			case "<C-u>":
+				search = ""
+			default:
+				if len(e.ID) == 1 {
+					search += e.ID
+				}
+			}
+
+			if previousSearch != search {
+				table.Rows = append([][]string(nil), table.Rows[:1]...)
+
+				if len(search) > 0 {
+					searchBar.Text = search
+					searchBar.TextStyle.Fg = ui.ColorClear
+
+					searchLowerCase := strings.ToLower(search)
+
+					for _, row := range rows {
+						if strings.Contains(strings.ToLower(row[0]), searchLowerCase) {
+							table.Rows = append(table.Rows, row)
+						}
+					}
+				} else {
+					searchBar.Text = "Search..."
+					searchBar.TextStyle.Fg = ui.ColorYellow
+
+					table.Rows = append(table.Rows, rows...)
+				}
+			}
+
+			previousSearch = search
+
+			newSelectedHost %= len(rows)
+
+			if newSelectedHost < 0 {
+				newSelectedHost = newSelectedHost + len(rows)
+			} else if newSelectedHost >= len(rows) {
+				newSelectedHost = newSelectedHost - len(rows)
+			}
+
+			if previousKey == "g" {
+				previousKey = ""
+			} else {
+				previousKey = e.ID
+			}
+
+			if newSelectedHost != selectedHost {
+				table.RowStyles[selectedHost+1] = ui.NewStyle(ui.ColorWhite, ui.ColorClear, ui.ModifierClear)
+				table.RowStyles[newSelectedHost+1] = ui.NewStyle(ui.ColorBlack, ui.ColorWhite, ui.ModifierClear)
+				selectedHost = newSelectedHost
+			}
+
+			ui.Render(grid)
+		}
+	},
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	flags := RootCmd.PersistentFlags()
+	flags.StringP("search", "s", "", "Host search filter")
+
+	viper.SetDefault("author", "quantumsheep <nathanael.dmc@outlook.fr>")
+	viper.SetDefault("license", "MIT")
+}
---
+++ go.mod
@@ -0,0 +1,33 @@
+module github.com/quantumsheep/sshs
+
+go 1.17
+
+require (
+	github.com/kevinburke/ssh_config v1.1.0
+	github.com/mikkeloscar/sshconfig v0.1.0
+)
+
+require (
+	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/gizak/termui/v3 v3.1.0 // indirect
+	github.com/hashicorp/hcl v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/magiconair/properties v1.8.5 // indirect
+	github.com/mattn/go-runewidth v0.0.2 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
+	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/spf13/afero v1.8.0 // indirect
+	github.com/spf13/cast v1.4.1 // indirect
+	github.com/spf13/cobra v1.3.0 // indirect
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/spf13/viper v1.10.1 // indirect
+	github.com/subosito/gotenv v1.2.0 // indirect
+	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/ini.v1 v1.66.3 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
