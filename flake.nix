94e823e102481a8845891dd7bf9268f1e8f57d51
Michal <michal@tar.black>
2022-07-11 00:37:52 +0100

    Cleared up flake versioning + flake fmt

--- flake.nix
+++ flake.nix
@@ -1,24 +1,27 @@
 {
   description = "Terminal user interface for SSH";
-  
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-  let
-    version = builtins.substring 0 8 self.lastModifiedDate;
-    pkgs = nixpkgs.legacyPackages.${system};
-  in
-    rec {
-      defaultPackage = pkgs.buildGoModule {
-        pname = "sshs";
-        inherit version;
-        src = ./.;
-        vendorSha256 = "QWFz85bOrTnPGum5atccB5hKeATlZvDAt32by+DO/Fo=";
-      };
-    }
-  );
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        version = "3.2.0-" + (self.shortRev or "dirty");
+        pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages.default = pkgs.buildGoModule {
+          pname = "sshs";
+          inherit version;
+          src = ./.;
+          vendorSha256 = "wClgX08UbItCpWOkWLgmsy7Ad5LlpvXrStN3JHujVww=";
+        };
+      }
+    );
 }
